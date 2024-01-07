b3e19d002047cb22c77e4df2b1894f0a8b455dae
kingYose <149393380+kingYose@users.noreply.github.com>
2024-01-07 12:21:46 +0200

    fix

--- db/conect.js
+++ db/conect.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
         host: 'localhost',
         user: 'root',
         database: 'place_holder',
-        password: '123456'
+        password: process.env.SQL_PASSWORD
 
 });
 
